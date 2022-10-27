9a1052adbfcad5b4500a72f69c2a91d060a3bf0a
Mamga <sjman1031@gmail.com>
2022-10-27 16:56:17 +0900

    2022.10.27 SJ 프로젝트 병합

---
+++ 2022_ProjectMarionette/Assets/Script/Data/DataManager.cs
@@ -0,0 +1,63 @@
+﻿using Marionette.Core;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace Marionette
+{
+    public struct Game_Data
+    {
+        public float handPower;
+        public float footPower;
+        public float moveSpeed;
+        public float waitTime;
+    }
+
+
+    public class DataManager : MonoBehaviour
+    {
+        private static DataManager instance = null;
+
+        private void Awake()
+        {
+            if (null == instance)
+            {
+                instance = this;
+
+                DontDestroyOnLoad(this);
+            }
+            else
+                Destroy(gameObject);
+        }
+
+
+        public static DataManager Instance
+        {
+            get
+            {
+                if (null == instance)
+                    return null;
+                return instance;
+            }
+        }
+
+        public Game_Data data;
+
+        private void Start()
+        {
+            string jsonData = Resources.Load("Data/Data").ToString();
+            data = JsonUtility.FromJson<Game_Data>(jsonData);
+        }
+
+        public void Load_Data()
+        {
+            Start();
+
+            GameManager.Instance.handPower = data.handPower;
+            GameManager.Instance.footPower = data.footPower;
+            GameManager.Instance.moveSpeed = data.moveSpeed;
+        }
+
+    }
+}
