f568c7f583a6cd2a9c604fc27a30a555558e6d83
Oonet96 <captaing96@gmail.com>
2022-11-04 17:28:37 +0900

    2022.11.04 HT : UIStartWindow 기본틀 작업 완료

---
+++ 2022_TeamProject_Marionette/Assets/Script/UI/02.Window/UIStartWindow.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using BaseFrame;
+
+namespace Marionette
+{
+    public class UIStartWindow : UIBase
+    {
+
+        protected override void setData(IUIOpenParam args)
+        {
+            base.setData(args);
+        }
+
+
+        #region Btn_Event
+        public void OnClickGameStart()
+        {
+            Debug.Log("Open UIMainWindow");
+            UIManager.Instance.CloseUI<UIStartWindow>();
+            UIManager.Instance.OpenUI<UIMainWindow>();
+        }
+        #endregion
+    }
+}
